cfe1c174708967a0f7ce2801c23852449581c735
Natalie T <natalietuzik@gmail.com>
2025-03-04 03:22:35 +0000

    square root sec

--- app.rb
+++ app.rb
@@ -21,4 +21,18 @@ get("/square/results") do
   erb(:square_results)
 end
 
-#square root sec
+# square root sec
+
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num **0.5
+
+  erb(:square_root_results)
+end
+
+# random sec
